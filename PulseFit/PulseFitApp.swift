1e491620497b1abce30ab5e04b444b89f91a08fb
Camilo <cami92008@hotmail.com>
2026-02-18 11:28:40 -0500

    Initial Commit

---
+++ PulseFit/PulseFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  PulseFitApp.swift
+//  PulseFit
+//
+//  Created by Camilo Montero on 2026-02-18.
+//
+
+import SwiftUI
+
+@main
+struct PulseFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
