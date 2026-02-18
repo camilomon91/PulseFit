cb66e4a4a0cc545542692e47aff229801b999171
Camilo <cami92008@hotmail.com>
2026-02-18 16:54:00 -0500

    supabase

---
+++ PulseFit/supabaseinfo.swift
@@ -0,0 +1,16 @@
+//
+//  supabaseinfo.swift
+//  PulseFit
+//
+//  Created by Camilo Montero on 2026-02-18.
+//
+
+
+import Foundation
+import Supabase
+
+let supabase = SupabaseClient(
+  supabaseURL: URL(string: "https://bhdvtvqckkisnhoisyel.supabase.co")!,
+  supabaseKey: "sb_publishable_EvcscqAa1L2m9jgm77Tw0Q_JDbGREK1"
+)
+        
